78b81fb457244e3ef50b0792d00b47a105d4f3bc
jdowell <jeremy@codevinsky.com>
2014-03-06 16:37:00 -0600

    dictionary

--- 3-6-2014/app.js
+++ 3-6-2014/app.js
@@ -1,16 +1,25 @@
 'use strict';
+var fs = require('fs'),
+  readline = require('readline'),
+  stream = require('stream'),
+  EventEmitter = require('events').EventEmitter;
 
 var CHAR_CODE_INDEX_ZERO = 65,
-key  = 'REDDIT',
-message = 'TODAYISMYBIRTHDAY';
+  mainKey  = 'REDDIT',
+  mainMessage = 'TODAYISMYBIRTHDAY',
+  vigenere = 'ZEJFOKHTMSRMELCPODWHCGAW',
+  decode = 'DAY',
+  counter = 0,
+  matchCounter = 0,
+  ee = new EventEmitter();
 
 function createModKey(k, m) {
-  var modKey = '', 
+  var modKey = '',
     i;
   for(i = 0; i < m.length; i++) {
     modKey += k[i % k.length];
   }
-  return modKey
+  return modKey;
 }
 
 function encodeMessage(k, m) {
@@ -46,8 +55,80 @@ function decypherMessage(k, c) {
   }
   return message;
 }
-var encoded = encodeMessage(key, message);
-console.log('key:\t\t\t', key);
-console.log('original message:\t', message);
+
+function getKeys() {
+  var instream = fs.createReadStream('dictionary.txt');
+  var outstream = new stream;
+  var keys = []
+  outstream.readable = true;
+  outstream.writable = true;
+
+
+  var rl = readline.createInterface({
+    input: instream,
+    output: outstream,
+    terminal: false
+  });
+
+  rl.on('line', function(line) {
+      keys.push(line.toUpperCase());
+  });
+
+  rl.on('close', function() {
+    ee.emit('keysAcquired', keys);
+  });
+}
+
+
+
+function crackCypher(c, maxKeyLength) {
+  var cyphers = [], matches = [], cracks = '';
+  getKeys(5);
+  console.log('cracking:', vigenere);
+  ee.on('keysAcquired', function(keys) {
+    keys.forEach(function(key) {
+      if(key.length <= maxKeyLength) {
+        cyphers.push({key: key, msg: decypherMessage(key, c), matches: 0});
+      }
+    });
+    cyphers.forEach(function(cypherObj) {
+      process.stdout.write(counter + '/' + cyphers.length + '\r');
+      counter++;
+      keys.forEach(function(key) {
+        if(key.length >= maxKeyLength && cypherObj.msg.indexOf(key) !== -1) {
+          cypherObj.matches++;
+          
+        }
+      });
+    });
+
+    cyphers.sort(function(a,b) { 
+      return a.matches - b.matches; 
+    });
+    cyphers.reverse();
+    var possibilities = cyphers.slice(0,24);
+    console.log('Top 25 Possibilities:');
+    possibilities.forEach(function(p) {
+      console.log(p.key,p.msg,p.matches);
+    });
+    cyphers.forEach(function(cypher) {
+      cracks += cypher.key + ' : ' + cypher.msg + ' : ' + cypher.matches + '\n';
+    });
+
+
+
+    fs.writeFile("cracked.txt", cracks, function(err) {
+      if(err)
+        throw(err);
+      else
+        console.log('all cracks written to cracked.txt.');
+    });
+  });
+}
+var encoded = encodeMessage(mainKey, mainMessage);
+console.log('key:\t\t\t', mainKey);
+console.log('original message:\t', mainMessage);
 console.log('encoded message:\t', encoded);
-console.log('decyphered message:\t', decypherMessage(key, encoded));
+console.log('decyphered message:\t', decypherMessage(mainKey, encoded));
+crackCypher(vigenere, 5);
+
