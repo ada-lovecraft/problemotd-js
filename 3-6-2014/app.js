a5151eb2d6b1ef83493422f1f00b6d2d3bf0e051
jdowell <jeremy@codevinsky.com>
2014-03-06 14:05:00 -0600

    Added decyphering function

--- 3-6-2014/app.js
+++ 3-6-2014/app.js
@@ -2,17 +2,22 @@
 
 var CHAR_CODE_INDEX_ZERO = 65,
 key  = 'REDDIT',
-message = 'TODAYISMYBIRTHDAY',
-encodeMessage;
+message = 'TODAYISMYBIRTHDAY';
 
-encodeMessage = function(k, m) {
-  var modKey = '',
-    cypher = '',
+function createModKey(k, m) {
+  var modKey = '', 
     i;
-
   for(i = 0; i < m.length; i++) {
     modKey += k[i % k.length];
   }
+  return modKey
+}
+
+function encodeMessage(k, m) {
+  var modKey, i,
+    cypher = '';
+  
+  modKey = createModKey(k,m);
 
   for(i = 0; i < m.length; i++) {
     var keyCode = modKey.charCodeAt(i) - CHAR_CODE_INDEX_ZERO;
@@ -21,6 +26,28 @@ encodeMessage = function(k, m) {
     cypher += String.fromCharCode(cypherCode + CHAR_CODE_INDEX_ZERO);
   }
   return cypher;
-};
+}
 
-console.log('cypher: ', encodeMessage(key, message));
+
+function decypherMessage(k, c) {
+  var modKey, 
+    message = '', 
+    i;
+
+  modKey = createModKey(k,c);
+  for(i = 0; i < c.length; i++) {
+    var cypherCode = c.charCodeAt(i) - CHAR_CODE_INDEX_ZERO;
+    var keyCode = modKey.charCodeAt(i) - CHAR_CODE_INDEX_ZERO;
+    var messageCode = (cypherCode - keyCode);
+    if(messageCode < 0) {
+      messageCode += 26;
+    } 
+    message += String.fromCharCode(messageCode+ CHAR_CODE_INDEX_ZERO);
+  }
+  return message;
+}
+var encoded = encodeMessage(key, message);
+console.log('key:\t\t\t', key);
+console.log('original message:\t', message);
+console.log('encoded message:\t', encoded);
+console.log('decyphered message:\t', decypherMessage(key, encoded));
